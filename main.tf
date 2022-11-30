87986f5741b80a06ac6386fa187693d42faf4c52
ujas3105 <ujasayodeji@gmail.com>
2022-11-30 17:09:07 +0100

    class-practise

--- main.tf
+++ main.tf
@@ -15,11 +15,79 @@ provider "aws" {
   
 }
 #creating vpc
-resource "aws_vpc" "ujas-the-devops-engineer-vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "ujas-vpc" {
+  cidr_block = var.cidr_block[0]
   tags= {
-    Name = "ujas-the-devops-engineer-vpc"
+    Name = "ujas-vpc"
+  }
+}
+
+#SUBNET
+resource "aws_subnet" "ujas-subnet" {
+  vpc_id     = aws_vpc.ujas-vpc.id
+  cidr_block = var.cidr_block[0]
+
+  tags = {
+    Name = "ujas-subnet"
   }
 }
 #provisioning of EC2
+resource "aws_instance" "My-apache-site" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+  key_name = "first-class"
+  subnet_id = aws_subnet.ujas-subnet.id
+  vpc_security_group_ids = [aws_security_group.Helloworld-sg.id]
+  associate_public_ip_address = true
+  user_data = file ("./mysite.sh")
+  tags = {
+    Name = "Mysite"
+  }
+}
 
+
+#security group
+resource "aws_security_group" "Helloworld-sg" {
+  name        = "allow_tls"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.ujas-vpc.id
+
+  dynamic ingress {
+    # description      = "Allow SSH from VPC"
+    iterator = port
+    for_each = var.ports
+    content{
+      from_port =port.value
+      to_port = port.value
+      cidr_blocks      = ["0.0.0.0/0"]
+      protocol         = "tcp"
+         }
+    # from_port        = 22
+    # to_port          = 22
+     
+  
+    # cidr_blocks      = [aws_vpc.main.cidr_block]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  # ingress {
+  #   description      = "Allow port 80 from VPC"
+  #   from_port        = 80
+  #   to_port          = 80
+  #   protocol         = "tcp"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  #   # cidr_blocks      = [aws_vpc.main.cidr_block]
+  #   # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  # }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
---
+++ mysite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+cd /tmp/
+wget https://www.tooplate.com/zip-templates/2121_wave_cafe.zip
+unzip 2121_wave_cafe
+cd 2121_wave_cafe
+cp -r * /var/www/html/
+systemctl restart httpd
+
